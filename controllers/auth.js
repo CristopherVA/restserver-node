c992b6f0f157b8849d1b235fad78c79701b91d34
CristopherVA <ing.valentin99@hotmail.com>
2021-08-29 14:59:47 -0400

    google signin added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcryptjs = require("bcryptjs");
 const { response } = require("express");
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 const login = async (req, res = response) => {
@@ -10,15 +11,15 @@ const login = async (req, res = response) => {
 
     try {
         // Verificar si el usuario existe
-        const usuario = await Usuario.findOne({correo})
-        if(!usuario){
+        const usuario = await Usuario.findOne({ correo })
+        if (!usuario) {
             return res.status(400).json({
                 msg: 'Usuario / Password no son correcto - correo'
             })
         }
 
         // Verifica si el usuario esta activo
-        if(!usuario.estado){
+        if (!usuario.estado) {
             return res.status(400).json({
                 msg: 'Usuario / Password no son correcto - estado: false'
             })
@@ -26,7 +27,7 @@ const login = async (req, res = response) => {
 
         // Verificar la password
         const verificarPassword = bcryptjs.compareSync(password, usuario.password)
-        if(!verificarPassword){
+        if (!verificarPassword) {
             return res.status(400).json({
                 msg: 'Usuario / Password no son correcto - password'
             })
@@ -42,10 +43,59 @@ const login = async (req, res = response) => {
 
     } catch (error) {
         console.log(error);
-        throw new Error ('Cominiquese con el administrador')
+        throw new Error('Cominiquese con el administrador')
     }
 }
 
+const googleSignin = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        const { nombre, correo, img } = await googleVerify(id_token)
+
+        let usuario = await Usuario.findOne({ correo })
+
+        if (!usuario) {
+
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true
+            }
+
+            usuario = new Usuario(data)
+            await usuario.save();
+        }
+
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador - usuario bloqueado'
+            })
+
+
+        }
+        
+        // Generar JWT
+        const token = await generarJWT(usuario.id)
+
+        return res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            msg: 'Token de google no es valido'
+        })
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignin
 }
--- models/usuario.js
+++ models/usuario.js
@@ -20,6 +20,7 @@ const UsuarioSchema = Schema({
     rol: {
         type: String,
         required: true,
+        default: 'USER_ROLE',
         emund: ['ADMIN_ROLE', 'USER_ROLE' ]
     },  
     estado:{
